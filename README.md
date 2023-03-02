b4e848018f820d7f822316c4a7d7c2d5f8745723
Roberto Larivera <robylariv@gmail.com>
2023-03-02 21:17:12 +0100

    Primi passi

---
+++ README.md
@@ -0,0 +1,4 @@
+# lc-personal-hello-mysql
+## Esercitazione personale per mysql
+
+### mysql & php 'puro' - primi passi
--- index.php
+++ index.php
@@ -1,36 +1,42 @@
 <?php
 
-define("DB_SERVERNAME","localhost:8889");
-define("DB_USERNAME","root");
-define("DB_PASSWORD","root");
-define("DB_NAME","db-university");
+define("DB_SERVERNAME", "localhost:8889");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "root");
+define("DB_NAME", "db-university");
 
 var_dump(DB_SERVERNAME);
 var_dump(DB_USERNAME);
 var_dump(DB_PASSWORD);
 var_dump(DB_NAME);
 
-$conn= new mysqli(DB_SERVERNAME,DB_USERNAME,DB_PASSWORD,DB_NAME);
+$conn = new mysqli(DB_SERVERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-
+echo '<h1> CONN </h1>';
 var_dump($conn);
 
 
-if($conn && $conn->connect_error){
+if ($conn && $conn->connect_error) {
     echo "Connection failed: " . $conn->connect_error;
-}
+} else {
+    $sql = "SELECT *  FROM departments";
 
-$sql = "SELECT room number, floor FROM stanze";
+    $result = $conn->query($sql);
+    echo '<h1> RESULT </h1>';
+    var_dump($result);
 
-$result = $conn->query( $sql) ;
-
-if ($result && $result->num_rows > 0) {
-    // output data of each row
-    while ($row = $result->fetch_assoc()) {
-    echo "Stanza N. ". $row[' room number']. " piano: ". $row[' floor'];
-    }
+    if ($result && $result->num_rows > 0) {
+        // output data of each row
+        echo '<h1> ROW </h1>';
+        while ($row = $result->fetch_assoc()) {
+            //echo "Stanza N. " . $row[' room number'] . " piano: " . $row[' floor'];
+            var_dump($row);
+        }
     } elseif ($result) {
-    echo "0 results";
+        echo "<h2>*** 0 results ***</h2>";
     } else {
-    echo "query error";
+        echo "<h2>*** query error ***</h2>";
     }
+}
+?>
+
